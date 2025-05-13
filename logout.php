a6bc2fda771fe714d03e217e08bdde0bc330c89c
Emi-Antunez <emilianoantunez@gmail.com>
2025-05-13 16:16:13 -0300

    cache

--- logout.php
+++ logout.php
@@ -1,5 +1,12 @@
 <?php
 session_start();
+session_unset();
 session_destroy();
+
+// Evitar mostrar caché luego del logout
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: 0");
+
 header("Location: index.php");
 exit();
