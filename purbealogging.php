e74f53bd29a0dc3838a441e902215e365a1c3506
Emi-Antunez <emilianoantunez13@gmail.com>
2025-08-05 13:14:23 -0300

    ..

---
+++ purbealogging.php
@@ -0,0 +1,6 @@
+<?php
+ini_set("display_errors", 0); // Desactiva error display
+ini_set("display_startup_errors", 0); // Desactiva startup errors
+ini_set('log_errors', 1); // Activa error logging
+ini_set('error_log', "Directory\mi_log_error,log");  //dirrectorio donde se almacenarán los logs de errores 
+?>
